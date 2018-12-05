06354d5edccddc6e16958c6f57e970c71959e4be
Yair Aviner <yaviner@gmail.com>
2018-12-04 20:35:45 -0500

    Add sendgrid test

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME
 });
 const bcrypt = require('bcryptjs');
+const sgMail = require('@sendgrid/mail');
 
 /*******************************************
 *   IMPORT MIDDLEWARE AND EXPRESS HELPERS  *
@@ -262,6 +263,19 @@ function requireLoggedIn(req, res, next) {
     }
     next();
 }
+
+// SENDGRID TEST
+app.get('/mailtest', function (res, req) {
+    sgMail.setApiKey('process.env.SENDGRID_API_KEY');
+    const msg = {
+        to: 'yaviner@gmail.com',
+        from: 'yaviner@teachingfoodblog.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+    sgMail.send(msg);
+})
 
 // 404 handler
 app.use(function (req, res, next) {
