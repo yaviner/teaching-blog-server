b911175137687e89c579ba4265dd1aac719ca13a
Yair Aviner <yaviner@gmail.com>
2018-12-05 10:11:09 -0500

    Comment out SG code.

--- server.js
+++ server.js
@@ -266,15 +266,16 @@ function requireLoggedIn(req, res, next) {
 
 // SENDGRID TEST
 app.get('/mailtest', function (res, req) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-        to: 'yaviner@gmail.com',
-        from: 'yaviner@teachingfoodblog.com',
-        subject: 'Sending with SendGrid is Fun',
-        text: 'and easy to do anywhere, even with Node.js',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-    sgMail.send(msg);
+    // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    // const msg = {
+    //     to: 'yaviner@gmail.com',
+    //     from: 'yaviner@teachingfoodblog.com',
+    //     subject: 'Sending with SendGrid is Fun',
+    //     text: 'and easy to do anywhere, even with Node.js',
+    //     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    // };
+    // sgMail.send(msg);
+    res.send('Nope, you can\'t send email.')
 })
 
 // 404 handler
